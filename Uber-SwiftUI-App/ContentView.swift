16fdc8efb52b6402bffde0206fab9abeb1e6f180
Sahil Agashe <sahil@scrambleapps.gmail.com>
2024-01-27 21:14:07 +0530

    Initial Commit

---
+++ Uber-SwiftUI-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uber-SwiftUI-App
+//
+//  Created by SAHIL AMRUT AGASHE on 27/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uber-SwiftUI-App/Uber_SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_SwiftUI_AppApp.swift
+//  Uber-SwiftUI-App
+//
+//  Created by SAHIL AMRUT AGASHE on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Uber_SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
